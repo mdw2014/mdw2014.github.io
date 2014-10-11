812d007462a7a5996c8e7b88c02d9a2e491ad118
maku693 <maku3ts@gmail.com>
2014-10-11 18:22:20 +0900

    Background canvas!!!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('#backgroundCanvas').width($(window).width).height($(window).height);
+  $('#backgroundCanvas').attr('width', $(window).width()).attr('height', $(window).height());
 
   $('.triangle').on('click', function(){
     $(this).parent().toggleClass('open');
@@ -36,7 +36,7 @@ $(function(){
     }
 
     resizing = setTimeout(function(){
-      $('#backgroundCanvas').width($(window).width).height($(window).height);
+      $('#backgroundCanvas').attr('width', $(window).width()).attr('height', $(window).height());
     }, 300);
   });
 
@@ -56,4 +56,58 @@ $(function(){
     }
   });
 
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+  var boids = [];
+  var canvas = document.getElementById('backgroundCanvas');
+  var ctx = canvas.getContext('2d');
+  var Boid = function(size, x, y, velX, velY) {
+    this.size = size;
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    return this;
+  };
+
+  function getRandomArbitary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function setup() {
+    for (var i = 0; i < 30; i++) {
+      var size = getRandomArbitary(canvas.width / 75, canvas.width / 30);
+      var x = getRandomArbitary(size, canvas.width - size);
+      var y = getRandomArbitary(size, canvas.height - size);
+      var velX = getRandomArbitary(-canvas.width / 400, canvas.width / 400);
+      var velY = getRandomArbitary(-canvas.width / 300, -canvas.width / 500);
+      boids.push(new Boid(size, x, y, velX, velY));
+    }
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#fff';
+    for (var i = 0; i < boids.length; i++) {
+      ctx.beginPath();
+      ctx.arc(boids[i].x, boids[i].y, boids[i].size, 0, Math.PI*2.0);
+
+      boids[i].x += boids[i].velX;
+      if (boids[i].x < -boids[i].size) boids[i] = canvas.width;
+      if (canvas.width + boids[i].size < boids[i].x) boids[i].x = -boids[i].size;
+
+      boids[i].y += boids[i].velY;
+      if (boids[i].y < -boids[i].size) {
+        boids[i].y = canvas.height + boids[i].size;
+        boids[i].x = getRandomArbitary(boids[i].size, canvas.width - boids[i].size);
+      }
+      ctx.fill();
+    }
+
+    requestAnimationFrame(draw);
+  }
+
+  setup();
+  draw();
 });
